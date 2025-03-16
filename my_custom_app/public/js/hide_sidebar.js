eea4eb7c7a1b1732fdce853252708a37b7aaa8cb
Ahmedalanee <32652568+Ahmedalanee@users.noreply.github.com>
2025-03-16 17:41:59 +0300

    Delete my_custom_app directory

--- my_custom_app/public/js/hide_sidebar.js
+++
@@ -1,9 +0,0 @@
-// Hide the sidebar in the desk
-frappe.ui.toolbar.Toolbar.prototype.make = function () {
-    this.page.addClass('hide-sidebar');
-};
-
-// Apply CSS to hide the sidebar
-frappe.ui.toolbar.Toolbar.prototype.setup_sidebar = function () {
-    $('.layout-side-section').hide();
-};
